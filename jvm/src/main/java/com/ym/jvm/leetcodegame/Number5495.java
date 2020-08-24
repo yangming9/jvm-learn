c8e00ce64d777f2516920a7a36417a8ebe08bd04
ym <yangming21_ok@13.com>
2020-08-24 16:47:29 +0800

    update

--- jvm/src/main/java/com/ym/jvm/leetcodegame/Number5495.java
+++ jvm/src/main/java/com/ym/jvm/leetcodegame/Number5495.java
@@ -5,12 +5,36 @@ import java.util.List;
 
 public class Number5495 {
     public static void main(String[] args) {
-
+        String s = "acdbbdaa";
+        System.out.println(longestPalindrome(s));
     }
-    public List<Integer> mostVisited(int n,int[] rounds){
-        List<Integer> list = new ArrayList<>();
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+        int strLen = s.length();
+        int maxStart = 0;  //最长回文串的起点
+        int maxEnd = 0;    //最长回文串的终点
+        int maxLen = 1;  //最长回文串的长度
 
+        boolean[][] dp = new boolean[strLen][strLen];
 
-        return list;
+        for (int r = 1; r < strLen; r++) {
+            for (int l = 0; l < r; l++) {
+                if (s.charAt(l) == s.charAt(r) && (r - l <= 2 || dp[l + 1][r - 1])) {
+                    dp[l][r] = true;
+                    if (r - l + 1 > maxLen) {
+                        maxLen = r - l + 1;
+                        maxStart = l;
+                        maxEnd = r;
+
+                    }
+                }
+
+            }
+
+        }
+        return s.substring(maxStart, maxEnd + 1);
+
     }
 }
