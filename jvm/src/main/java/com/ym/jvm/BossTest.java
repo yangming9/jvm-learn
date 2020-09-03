228fd624ff69507cf0cb88fb6ec99100d52b50a9
ym <yangming21_ok@13.com>
2020-09-03 17:04:37 +0800

    update

--- jvm/src/main/java/com/ym/jvm/BossTest.java
+++ jvm/src/main/java/com/ym/jvm/BossTest.java
@@ -1,9 +1,9 @@
 package com.ym.jvm;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
 
 public class BossTest {
     List getFast(){
@@ -25,40 +25,46 @@ public class BossTest {
         }).start();
 
 
-        List result = queue.poll();
+         result = queue.poll();
 
         return result;
     }
 
-    List a(){}
-    List b(){}
-    List c(){}
+    List a(){
+        return new ArrayList();
+    }
+    List b(){
+        return new ArrayList();
+    }
+    List c(){
+        return new ArrayList();
+    }
 
 
 
-    score 成绩表
-
-    id 主键
-    class_id 班级
-    name  学生名
-    course 课程
-    score 分数
-    test_date8 测试的日期
-
-1. 查询班级A的 某一天考试中 各科成绩最好学生名、课程、和成绩
-
-
-    语文 章三 100
-    语文 里斯 100
-    数学 章三 100
-    数学 里斯 100
-
-
-    查询字段：
-
-    select s.course,s.name,s.score from score s inner join
-    (select course,max(score) score from score where class_id = "A" and test_date8 = ? group by course) a
-    on s.score = a.score and s.course = a.course and class_id = "A" and test_date8 = ?
+//    score 成绩表
+//
+//    id 主键
+//    class_id 班级
+//    name  学生名
+//    course 课程
+//    score 分数
+//    test_date8 测试的日期
+//
+//1. 查询班级A的 某一天考试中 各科成绩最好学生名、课程、和成绩
+//
+//
+//    语文 章三 100
+//    语文 里斯 100
+//    数学 章三 100
+//    数学 里斯 100
+//
+//
+//    查询字段：
+//
+//    select s.course,s.name,s.score from score s inner join
+//    (select course,max(score) score from score where class_id = "A" and test_date8 = ? group by course) a
+//    on s.score = a.score and s.course = a.course and class_id = "A" and test_date8 = ?
 
 
 
