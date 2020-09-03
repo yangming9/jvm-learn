11ee368da2763dfbc08c11964d7316376bbaf9b5
ym <yangming21_ok@13.com>
2020-09-03 16:59:29 +0800

    update

---
+++ jvm/src/main/java/com/ym/jvm/BossTest.java
@@ -0,0 +1,65 @@
+package com.ym.jvm;
+
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class BossTest {
+    List getFast(){
+
+        List result;
+
+        Queue<List> queue = new LinkedBlockingQueue<>();
+
+        new Thread(() -> {
+            queue.offer(a());
+        }).start();
+
+        new Thread(() -> {
+            queue.offer(b());
+        }).start();
+
+        new Thread(() -> {
+            queue.offer(c());
+        }).start();
+
+
+        List result = queue.poll();
+
+        return result;
+    }
+
+    List a(){}
+    List b(){}
+    List c(){}
+
+
+
+    score 成绩表
+
+    id 主键
+    class_id 班级
+    name  学生名
+    course 课程
+    score 分数
+    test_date8 测试的日期
+
+1. 查询班级A的 某一天考试中 各科成绩最好学生名、课程、和成绩
+
+
+    语文 章三 100
+    语文 里斯 100
+    数学 章三 100
+    数学 里斯 100
+
+
+    查询字段：
+
+    select s.course,s.name,s.score from score s inner join
+    (select course,max(score) score from score where class_id = "A" and test_date8 = ? group by course) a
+    on s.score = a.score and s.course = a.course and class_id = "A" and test_date8 = ?
+
+
+
+}
--- jvm/src/main/java/com/ym/jvm/JvmApplication.java
+++ jvm/src/main/java/com/ym/jvm/JvmApplication.java
@@ -1,36 +1,13 @@
 package com.ym.jvm;
 
-import com.ym.jvm.task.TestTask;
-import com.ym.jvm.threads.ThreadUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 public class JvmApplication {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(JvmApplication.class, args);
-        //测试线程池任务执行
-        ThreadPoolExecutor executor = ThreadUtils.getThreads();
-        Future<?> future = executor.submit(new TestTask());
-        future.get();
-        int n = 345;
-        int length = (n + "").length();
-        String str = n + "";
-        ArrayList<Character> list = new ArrayList<>();
-        list.add(str.charAt(length - 1));
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = list.size() - 1; i >= 0; i--) {
-			stringBuilder.append(list.get(i));
-        }
-
     }
-
 
 }
